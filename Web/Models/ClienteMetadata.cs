434e0461f4dce65d65029b7b37e218aa7e99f386
psanchez <psanchez@plus54.com>
2018-05-09 12:58:59 -0300

    agregando customvalidation emailunico

--- Web/Models/ClienteMetadata.cs
+++ Web/Models/ClienteMetadata.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace EF.Validaciones.Models
 {
@@ -18,6 +19,28 @@ namespace EF.Validaciones.Models
 
         [Required]
         [EmailAddress]
+        [CustomValidation(typeof(ClienteMetadata), "ValidarEmailUnico")]
         public string Email { get; set; }
+
+        public static ValidationResult ValidarEmailUnico(object value, ValidationContext context)
+        {                 
+            var cliente = context.ObjectInstance as Cliente;
+
+            if (cliente == null || string.IsNullOrEmpty(cliente.Email))
+                return new ValidationResult(string.Format("Email es requerido."));
+
+            //para validar que no exista otro email igual, debo chequear en la base
+            ClientesEntities db = new ClientesEntities();
+
+            var existeEmail = db.Clientes.Any(o=> o.Email == cliente.Email);
+
+            if (existeEmail)
+            {
+                return new ValidationResult(string.Format("El Email {0} ya está siendo usado.", cliente.Email));
+            }
+
+            return ValidationResult.Success;
+        }
+
     }
 }
